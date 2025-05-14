952884b5540a349597632cabee553cadffc5743a
jonas3688 <joaocapozzi60@gmail.com>
2025-05-14 15:26:15 -0400

    Implementação do método que recebe o token do git

--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect 
 #from .models import user 
 from . import settings
+import 
 
 
 def api_auth_git(request):
     gitCode = request.GET.get('code')
     request.post(f"https://github.com/login/oauth/access_token?client_id={settings.GITHUB_CLIENT_ID}&client_secret{settings.GITHUB_CLIENT_SECRET}&code={gitCode}&redirect_uri=http://localhost:8000/api/auth/github")
+
+    acessToken_recieved = request.post(
+    "https://github.com/login/oauth/access_token",
+    data={
+
+        #parametros que o github exige para conseguirmos o token 
+        'client_id': settings.GITHUB_CLIENT_ID,
+        'client_secret': settings.GITHUB_CLIENT_SECRET,
+        'code': gitCode,
+        'redirect_uri': 'http://localhost:8000/api/auth/github',
+    },
+    headers={'Accept': 'application/json'}
+)
+
 
     #precisa retornar um JWT agora depois que autentiar o token
 
