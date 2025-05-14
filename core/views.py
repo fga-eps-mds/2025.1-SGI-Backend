ef7c56a17d43c4168a076030f1a2824def6150aa
jonas3688 <joaocapozzi60@gmail.com>
2025-05-14 14:00:10 -0400

    Inicio dos metodos de requisição GIT

--- core/views.py
+++ core/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect 
-from .models import user
+#from .models import user 
+from . import settings
 
-def callback(request):
+
+def api_auth_git(request):
+    gitCode = request.GET.get('code')
+    request.post(f"https://github.com/login/oauth/access_token?client_id={settings.GITHUB_CLIENT_ID}&client_secret{settings.GITHUB_CLIENT_SECRET}&code={gitCode}&redirect_uri=http://localhost:8000/api/auth/github")
+
+    #precisa retornar um JWT agora depois que autentiar o token
+
+def login(request):
+    request.get(f"https://github.com/login/oauth/authorize?client_id={settings.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8000/api/auth/github")
+    
+def index():
     pass
-
-def authLogin(request):
-    if request.method == 'GET':
-        pass 
-    if request.method == 'POST':
-        pass 
