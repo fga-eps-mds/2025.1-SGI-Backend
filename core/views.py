5444cb1a39856bdf1867f3e2cd7a9b45a4733ecc
JoaoGuilherme14 <232014039@aluno.unb.br>
2025-05-14 16:53:19 -0300

    Corrgi a função login e adicionei o access_token

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect 
 #from .models import user 
 from . import settings
-import 
 
 
 def api_auth_git(request):
@@ -20,12 +19,16 @@ def api_auth_git(request):
     },
     headers={'Accept': 'application/json'}
 )
-
+    token_data = acessToken_recieved.json()
+    access_token = token_data.get('access_token')
+    #access_token pega o token do git
 
     #precisa retornar um JWT agora depois que autentiar o token
 
 def login(request):
-    request.get(f"https://github.com/login/oauth/authorize?client_id={settings.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8000/api/auth/github")
-    
+    github_auth_url = ("https://github.com/login/oauth/authorize?client_id={settings.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8000/api/auth/github")
+    return redirect(github_auth_url)
+    #redireciona para o GitHUb depois redireciona para a pagina de login
+
 def index():
     pass
