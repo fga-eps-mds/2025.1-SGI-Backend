7b0d23e15c18b24639d41c70b8af4ca9cc4ed055
cvbmiranda <vilinhasgames123@gmail.com>
2025-05-17 00:38:12 -0300

    adiciona apps rest_framework, simplejwt e social_django ao INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # apps para autenticação e API
-    #'rest_framework',
-    #'rest_framework_simplejwt',
-    #'social_django',
+    # apps para autenticação e API (usando JWT)
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'social_django',
 ]
 
 MIDDLEWARE = [
