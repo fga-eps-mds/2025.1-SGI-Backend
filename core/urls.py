fbf71d6918c8b05637a0c1f39870d60dc258bc14
jonas3688 <joaocapozzi60@gmail.com>
2025-06-16 18:37:56 -0300

    total_commits enpoint creation

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('api/auth/github', views.git_auth_code, name='git_auth_code'),
-    path('callback', views.git_auth_token, name='callback')
+    path('callback', views.git_auth_token, name='callback'),
+    
+    path('/api/users/<int:id>/total_commits',views.total_commits,name='total_commits')
 ]
