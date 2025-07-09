ec051c4d2b9e29c52f6a6e565b5a9aeb51fe6d72
jonas3688 <joaocapozzi60@gmail.com>
2025-07-09 01:24:10 -0300

    Criação testes.py

---
+++ core/tests.py
@@ -0,0 +1,14 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import JsonResponse
+#from views import blacklist 
+
+class TestsGitFIca(APITestCase):
+    
+    #inicialização e configuração base pros testes do django test
+    def setUp(self):
+        
+        #self.user = User.objects.create_user(username='usuarioteste123', password='testeteste123')
+        self.client = APIClient()
