8d5fd08ae635eaee1e2e3e09bb3fcf5a41cbe829
jonas3688 <joaocapozzi60@gmail.com>
2025-07-09 00:30:43 -0300

    criação teste token válido blacklist

--- core/tests.py
+++ core/tests.py
@@ -11,8 +11,17 @@ class TestsGitFIca(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='usuarioteste123', password='testeteste123')
         self.client = APIClient()
-        
+    
+    #teste pra quando o blacklist recebe um gwt invalido
     def blacklist_false_token(self):
         invalid_token = 'testetesteteste'
         result = blacklist(None, invalid_token)
         self.assertFalse(result)
+        
+    #teste pra jwt valido 
+    def blacklist_token_correct(self):
+        testJwt = RefreshToken.for_user(self.user) #Essa função vai emular um jwt válido pra testar se o blacklist ta funcionando certinho 
+        result = blacklist(None, str(testJwt))
+        self.assertTrue(result)
+        
+        
