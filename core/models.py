d13e94c3189cbebb64042a69f75eeaa1d95044a8
cvbmiranda <vilinhasgames123@gmail.com>
2025-07-09 15:33:52 -0300

    feat(Profile): adicionei a __str__ com info de usuário e pontuação

--- core/models.py
+++ core/models.py
@@ -5,4 +5,6 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # Score based on Commit activities
     pontuacao_commits = models.IntegerField(default=0)
-
+    
+    def __str__(self):
+        return f"Profile de {self.user.username} - Pontuação: {self.pontuacao_commits}"
