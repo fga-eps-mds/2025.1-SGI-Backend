a425bef68cc60af4b71696ab880343d2af655340
jonas3688 <joaocapozzi60@gmail.com>
2025-07-10 18:18:53 -0300

    Criação teste user encontrado

--- core/tests.py
+++ core/tests.py
@@ -24,3 +24,21 @@ class TestsGitFIca(TestCase):
         response = self.client.get('/api/users/teste/')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json()['error'], 'User not found')
+        
+    #teste para quando a api é chamada e encontra um usuario
+    @patch('core.views.requests.get')
+    def test_github_user_found(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            'name': 'userteste',
+            'avatar_url': './avatar.png',
+            'bio': 'teste'
+        }
+
+        response = self.client.get('/api/users/userteste/')  # username simulado
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['name'], 'userteste')
+        self.assertEqual(data['avatar_url'], './avatar.png')
+        self.assertEqual(data['bio'], 'teste')
